0084ed568a3c1321e2907bf635214685b764d543
binperson <binperson@binpersondeMacBook-Pro.local>
2020-09-02 15:22:58 +0800

    chore:修改配置

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -366,6 +366,11 @@ module.exports = function(webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
+            {
+              test: /\.(glsl|vs|fs)$/,
+              include: paths.appSrc,
+              loader: require.resolve('ts-shader-loader')
+            },
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
